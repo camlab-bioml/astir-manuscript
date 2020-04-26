2fe9320f25570e4fe385e63bb8999b8d8f3a6033
kieranrcampbell <kieranrcampbell@gmail.com>
2020-04-26 13:59:03 -0400

    Add explanatory comment to setup-galen.sh

--- setup-galen.sh
+++ setup-galen.sh
@@ -1,3 +1,9 @@
+#!/bin/bash
+
+# This bash script creates data-raw & corresponding subdirectories in the current directory
+# and creates symbolic links from mass cytometry data stored in /home/ltri/campbell/share/datasets/
+# to be used as raw data input
+
 mkdir -p data-raw
 mkdir -p data-raw/wagner-2019
 
