13f1bae7c6755f2fdadd819f9cf79e885bbb8302
kieranrcampbell <kieranrcampbell@gmail.com>
2020-04-26 13:57:23 -0400

    Add setup-galen.sh

---
+++ setup-galen.sh
@@ -0,0 +1,6 @@
+mkdir -p data-raw
+mkdir -p data-raw/wagner-2019
+
+ln -s /home/ltri/campbell/share/datasets/jackson-imc/ data-raw/
+ln -s /home/ltri/campbell/share/datasets/wagner-2019-cell/fcs_files data-raw/wagner-2019/fcs_files
+
