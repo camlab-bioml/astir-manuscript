b87b82bab8eacef7103e5aa4bbd956d83a996962
kieranrcampbell <kieranrcampbell@gmail.com>
2020-07-06 12:59:57 -0400

    Add benchmarking v2

---
+++ pipeline/benchmarking/benchmark-gsva-methods2.R
@@ -0,0 +1,164 @@
+
+suppressPackageStartupMessages({
+  library(tidyverse)
+  library(GSVA)
+  library(argparse)
+  library(yaml)
+})
+
+
+parser <- ArgumentParser(description = "Benchmark GSVA timings")
+
+parser$add_argument('--input_dir', type = 'character')
+parser$add_argument('--markers', type = 'character')
+parser$add_argument('--n_cells', type = 'integer')
+parser$add_argument('--method', type = 'character')
+parser$add_argument('--dataset', type = 'character')
+parser$add_argument('--output_file', type = 'character')
+parser$add_argument('--max_epochs', type='integer')
+parser$add_argument('--batch_size', type='integer')
+parser$add_argument('--learning_rate', type='double')
+parser$add_argument('--n_initial_epochs', type='integer')
+
+args <- parser$parse_args()
+
+markers <- read_yaml(args$markers)
+
+csv_files <- dir(args$input_dir, pattern='csv', full.names = TRUE)
+csv_files <- sample(csv_files) # randomize
+
+
+expr_csv <- read_csv(csv_files[1])
+i = 2
+
+while(i < length(csv_files) && nrow(expr_csv) < args$n_cells) {
+  expr_csv <- rbind(
+    expr_csv,
+    read_csv(csv_files[i])
+  )
+  i <- i+1
+}
+
+expr_csv <- sample_n(expr_csv, min(args$n_cells, nrow(expr_csv)))
+
+g <- st <- expr_mat <- NULL
+
+if("X" %in% names(expr_csv)) {
+  expr_csv <- select(expr_csv, -X)
+}
+
+if("Y" %in% names(expr_csv)) {
+  expr_csv <- select(expr_csv, -Y)
+}
+
+expr_mat <- select(expr_csv, -X1) %>% 
+  as.matrix() %>% 
+  t()
+
+pathways_to_test <- markers$cell_states
+pathways_to_test <- pathways_to_test[sapply(pathways_to_test, length) > 2]
+
+heldout_proteins <- lapply(
+  pathways_to_test,
+  sample,
+  size = 1
+)
+
+modified_markers <- lapply(names(pathways_to_test), function(pathway) {
+  setdiff(
+    pathways_to_test[[pathway]],
+    heldout_proteins[[pathway]]
+  )
+})
+
+lens <- sapply(markers$cell_types, length)
+if(any(lens == 1)) {
+  for(i in which(lens == 01)) {
+    markers$cell_types[[i]] <- list(markers$cell_types[[i]])
+  }
+}
+
+names(modified_markers) <- names(pathways_to_test)
+
+if(args$method != "astir") {
+  st <- system.time({
+    g <- gsva(expr_mat, 
+              modified_markers,
+              method = args$method)
+  })
+} else {
+  csv_file <- tempfile(tmpdir='tmp')
+  output_file <- tempfile(tmpdir='tmp')
+  expr_csv %>% 
+    write_csv(csv_file)
+  
+  yaml_file <- tempfile(tmpdir='tmp')
+  markers$cell_states <- modified_markers
+  write_yaml(markers, yaml_file)
+
+  cmd <- paste("/home/ltri/campbell/kcampbel/.conda/envs/imc/bin/python",
+              "/home/ltri/campbell/kcampbel/.conda/envs/imc/bin/astir",
+              "state",
+              csv_file,
+              yaml_file,
+              output_file,
+              "--max_epochs", args$max_epochs,
+              "--batch_size", args$batch_size,
+              "--learning_rate", args$learning_rate,
+              "--n_init_epochs", args$n_initial_epochs
+  )
+
+
+  st <- system.time({
+    system(cmd)
+  })
+  
+  output <- read_csv(output_file)
+  output <- select(output, -X1)
+  g <- t(as.matrix(output))
+  
+} 
+
+get_stat <- function(pathway) {
+# p <- names(modified_markers)[1]
+  expression <- expr_mat[heldout_proteins[[pathway]],  ]
+  pathway_score <- g[pathway,]
+  
+  fit <- lm(expression ~ pathway_score)
+  s <- summary(fit)
+  list(
+    statistic = s$coefficients[2,3],
+    coefficient = s$coefficients[2,1],
+    correlation = cor(expression, pathway_score)
+  )
+}
+stats <- lapply(
+  names(modified_markers),
+  get_stat
+)
+
+statistics <- sapply(stats, `[[`, 'statistic')
+coefs <- sapply(stats, `[[`, 'coefficient')
+correlations <- sapply(stats, `[[`, 'correlation')
+
+df_res <- tibble(
+  method = args$method,
+  dataset = args$dataset,
+  n_cells = args$n_cells,
+  statistics = statistics,
+  coefficients = coefs,
+  correlations = correlations
+)
+
+# 
+# cors <- sapply(names(markers$cell_states), function(p) {
+#   c1 <- t(g[p,,drop=FALSE])
+#   c2 <- t(expr_mat[markers$cell_states[[ p ]], ])
+#   (cor(c1,c2))
+# }, simplify = FALSE)
+# 
+# cors <- unlist(cors)
+
+
+
+write_csv(df_res, args$output_file)
--- pipeline/benchmarking/benchmarking.smk
+++ pipeline/benchmarking/benchmarking.smk
@@ -5,7 +5,7 @@
 
 
 repeats = 5
-gsva_methods = ["gsva", "ssgsea", "plage", "astir"]
+gsva_methods = ["plage", "astir"] # "gsva", "ssgsea", 
 
 n_cells = [1000, 5000, 10000, 50000]
 
@@ -14,13 +14,13 @@ datasets = ['basel', 'zurich1']
 output_path_benchmarking = output_path + "benchmarking/geneset/"
 
 geneset_files = expand(output_path_benchmarking + "geneset_benchmark_{d}_{m}_{n}_{r}.csv",
-                d=datasets,
+                d=['basel','zurich1','schapiro'],
                 m=gsva_methods, n=n_cells, r = range(repeats))
 
-geneset_files = geneset_files + \
-    expand(output_path_benchmarking + "geneset_benchmark_{d}_{m}_{n}_{r}.csv",
-            d=datasets,
-            m=['plage', 'astir'], n=[50000,100000,200000], r = range(repeats))
+# geneset_files = geneset_files + \
+#     expand(output_path_benchmarking + "geneset_benchmark_{d}_{m}_{n}_{r}.csv",
+#             d=['basel', 'zurich1'],
+#             m=['plage', 'astir'], n=[50000,100000,200000], r = range(repeats))
 
 benchmarking_output = {
     'files': geneset_files
@@ -34,11 +34,11 @@ rule run_gsva_benchmark:
         n_initial_epochs = config['astir_opts']['n_initial_epochs'],
     input:
         all_csvs = directory(output_path + "{d}_processed/"),
-        markers = "data-raw/jackson-2020-markers.yml"
+        markers = lambda wildcards: config[wildcards.d]['marker_file']
     output:
         output_path_benchmarking + "geneset_benchmark_{d}_{m}_{n}_{r}.csv"
     shell:
-        "Rscript pipeline/benchmarking/benchmark-gsva-methods.R "
+        "Rscript pipeline/benchmarking/benchmark-gsva-methods2.R "
         "--input_dir {output_path}/{wildcards.d}_processed/ "
         "--markers {input.markers} "
         "--n_cells {wildcards.n} "
