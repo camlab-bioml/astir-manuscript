899e64856162754382df973b786caed27ac4fad2
Michael-Geuenich <michaelgeuenich@yahoo.com>
2020-11-29 20:34:25 -0500

    change cell type proportions to basel

--- pipeline/epithelial-overclustering/create-cell-type-proportion-datasets.R
+++ pipeline/epithelial-overclustering/create-cell-type-proportion-datasets.R
@@ -2,6 +2,7 @@
 
 library(tidyverse)
 library(devtools)
+library(DelayedArray)
 devtools::load_all("../taproom/")
 
 args <- commandArgs(trailingOnly = TRUE)
@@ -9,27 +10,29 @@ astir_assignments <- args[1]
 percentage_luminal <- args[2]
 output_dir <- args[3]
 
+astir_assignments <- "output/chipmunk/astir_assignments/basel_astir_assignments.csv"
+
 # Read in data & remove other, Unknown & basal cells
-wagner_assignments <- read_csv(astir_assignments) %>% 
+assignments <- read_csv(astir_assignments) %>% 
   column_to_rownames("X1")
 
-wagner_assignments$cell_type <- get_celltypes(wagner_assignments)
-wagner_assignments <- filter(wagner_assignments, 
-                             !cell_type %in% c("Unknown", "Other", "Epithelial (basal)"))
+assignments$cell_type <- get_celltypes(assignments)
+assignments <- filter(assignments, 
+                      !cell_type %in% c("Unknown", "Other"))
 
 # which cell type has the lowest number of cells? (this will be the max # of
 # cells for any group if I want to retain equal proportions)
-min_cells <- table(wagner_assignments$cell_type) %>% as.numeric() %>% min()
+min_cells <- table(assignments$cell_type) %>% as.numeric() %>% min()
 
 # How many cell types do we have represented?
-cell_type_number <- length(unique(wagner_assignments$cell_type))
+cell_type_number <- length(unique(assignments$cell_type))
 
 # total number of cells per experiment
 cell_number_per_sample <- min_cells * cell_type_number
 
 
 get_cell_names <- function(type, number){
-  wagner_assignments %>% 
+  assignments %>% 
     filter(cell_type == type) %>% 
     slice(1:number) %>% 
     rownames_to_column("cell_ID") %>%
@@ -38,7 +41,8 @@ get_cell_names <- function(type, number){
 
 
 ### CREATE ANY SAMPLE FUNCTION #########
-cell_types_wout_luminal <- c("Endothelial", "Macrophage", "Stromal", "T cells")
+cell_types_wout_luminal <- c("Epithelial (other)", "Macrophage", "Stromal", 
+                             "Endothelial", "T cells", "B cells", "Epithelial (basal)")
 
 create_sample <- function(percent_luminal){
   # Calculate number of cells for each type
@@ -48,7 +52,7 @@ create_sample <- function(percent_luminal){
   # Create subset of non luminal cells
   other_cells <- lapply(cell_types_wout_luminal, get_cell_names, number = other_no) %>% 
     bind_cols()
-  colnames(other_cells) <- cell_types_no_luminal
+  colnames(other_cells) <- cell_types_wout_luminal
   
   other_cells <- other_cells %>% 
     pivot_longer(everything(), names_to = "type", values_to = "cell_id")
@@ -62,8 +66,8 @@ create_sample <- function(percent_luminal){
 
 
 
-luminal_99 <- create_sample(percentage_luminal)
-write_csv(luminal_99, file = paste0(output_dir, "luminal_", percentage_luminal, ".csv"))
+luminal_99 <- create_sample(percent_luminal)
+write_csv(luminal_99, file = paste0(output_dir, "luminal-", percentage_luminal, ".csv"))
 
 # luminal_80 <- create_sample(0.8)
 # write_csv(luminal_80, file = "output/chipmunk/results/epithelial_overclustering/luminal_80.csv")
