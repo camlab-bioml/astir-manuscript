a200b58aa4cbe3d90781b2ca84678e93f5e7d3cd
Michael-Geuenich <michaelgeuenich@yahoo.com>
2021-03-30 17:06:02 -0400

    save legacy scripts

--- pipeline/analysis/wagner-tsne-plots.R
+++ pipeline/analysis/wagner-tsne-plots.R
@@ -1,10 +1,10 @@
 library(tidyverse)
 library(devtools)
-devtools::load_all("../../../taproom/")
+devtools::load_all("../taproom/")
 
-markers <- read_markers("../../markers/wagner-2019-markers-v4.yml")
+markers <- read_markers("markers/wagner-2019-markers-v4.yml")
 
-tsne <- read_csv("../../output/squirrel/results/wagner_tsne_expression-subset.csv") %>% 
+tsne <- read_csv("output/squirrel/results/wagner_tsne_expression-subset.csv") %>% 
   as.data.frame()
 
 ### Cell type colouring
@@ -22,7 +22,7 @@ tsne_longer <- tsne %>%
 ### Marker colouring
 ggplot(tsne_longer, aes(x = `tSNE Dim1`, y = `tSNE Dim2`)) +
   geom_point(aes(color = expression, alpha = 0.001)) +
-  scale_color_viridis_c() +
+  scale_color_viridis_c(name = "Expression") +
   #scale_color_manual(values = jackson_basel_colours(), name = "Cell type") +
   scale_alpha(guide = 'none') +
   astir_paper_theme() +
--- pipeline/epithelial-overclustering/create-UMAPs.R
+++ pipeline/epithelial-overclustering/create-UMAPs.R
@@ -8,15 +8,15 @@ cells <- read_csv(args[1])
 expression <- readRDS(args[2])[,cells$cell_id]
 out_dir <- args[3]
 markers <- args[4]
-markers_list <- read_markers(args[5])
 
-if(markers == "specified_markers"){
-  expression <- expression[unique(unlist(markers$cell_types)),]
-}
+cells <- read_csv("output/chipmunk/results/epithelial_overclustering/luminal_80.csv")
+out_dir <- "output/chipmunk/results/epithelial_overclustering/"
+expression <- readRDS("output/chipmunk/sces/wagner_sce.rds")[,cells$cell_id]
+markers <- "all_markers"
 
 expression <- runUMAP(expression)
 
 umap <- reducedDim(expression) %>% as.data.frame() %>% rownames_to_column("cell_id")
 colnames(umap) <- c("cell_id", "UMAP 1", "UMAP 2")
 
-write_csv(umap, paste0(out_dir, "UMAPs/UMAP-luminal-80-", markers, ".csv"))
+write_csv(umap, paste0(out_dir, "UMAPs/UMAP-luminal-0-", markers, ".csv"))
